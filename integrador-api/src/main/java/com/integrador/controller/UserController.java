5a99e86b6b742a852398538adcd3400cdf483837
ryan <rsdq0101@gmail.com>
2024-09-04 16:45:56 -0300

    conferir

--- integrador-api/src/main/java/com/integrador/controller/UserController.java
+++ integrador-api/src/main/java/com/integrador/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.integrador.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.integrador.interfaces.UserJPA;
@@ -18,11 +24,17 @@ public class UserController
 	UserController( UserJPA repository )
 	{
 		this.data = repository;
-	}
+	}// end UserController
 	
-	@PostMapping
-	public void create( @RequestBody User newUser )
+	@PostMapping @ResponseStatus( code = HttpStatus.CREATED )
+	void create( @RequestBody User newUser )
 	{
 		data.save( newUser );
+	}
+	
+	@GetMapping( "{account}" )
+	List<User> searchForUsers( @PathVariable String account )
+	{
+		return data.findByaccountName( account );
 	}
 }
--- integrador-api/src/main/java/com/integrador/model/User.java
+++ integrador-api/src/main/java/com/integrador/model/User.java
@@ -1,7 +1,5 @@
 package com.integrador.model;
 
-import com.integrador.services.IdNumber;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -17,17 +15,53 @@ public class User
 	@Id @GeneratedValue( strategy = GenerationType.IDENTITY )
 	private Long ID;
 	
-	@Column( nullable = false, length = 150 )
-	private String name;
+	@Column( nullable = false, length = 150, name = "FIRST_NAME" )
+	private String first_name;
 	
-	@Column( nullable = false, length = 11 )
-	private IdNumber personalID;
+	@Column( nullable = false, length = 150, name = "LAST_NAME" )
+	private String last_name;
 	
-	User( String str, IdNumber id )
+	@Column( nullable = false, length = 100, name = "ACCOUNT_NAME" )
+	private String accountName;
+
+	@Column( nullable = false, length = 100, unique = true )
+	private String email;
+	
+	@Column( nullable = false, length = 100 )
+	private String password;
+	
+	User( String f_name, String l_name, String ac_name, String email, String pass )
 	{
-		this.personalID = id;
-		this.name = str;		
-	}
+		this.first_name = f_name;
+		this.last_name = l_name;
+		this.accountName = ac_name;
+		this.email = email;
+		this.password = pass;
+	}// end User
 	
-	public String getName() { return this.name; }
+	User(){}
+
+	public Long getID() {
+		return ID;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
 }
